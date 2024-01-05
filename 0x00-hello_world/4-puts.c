5f7b4e841172c04adee0b679d252726e670c64f6
Sherry <cheronlestrange@gmail.com>
2024-01-05 02:09:47 -0800

    fix 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-int main ()
+/**
+ * main - Entry point
+ *
+ * Description: Using puts to print string
+ * @parameter: string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("Programming is like building a multilngual puzzle");
-	
-	return 0;
-}	
+	return (0);
+}
